2d955f6b78fe4fb100b44e41aadf4a8860193aab
abatchanga <125188051+abatchanga@users.noreply.github.com>
2023-11-22 07:03:20 -0500

    Update import.tf

--- app6-import-lost-state/import.tf
+++ app6-import-lost-state/import.tf
@@ -1,4 +1,5 @@
 # We can also use the terraform import bloc to recover a lost state file 
+# What will you do if you lose your state file? You import your resource
 
 import {
   to = aws_iam_group.demo
